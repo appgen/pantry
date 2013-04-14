599238ac05ed8d7310203dcc29b645aa5cf87192
Thomas Levine <occurrence@thomaslevine.com>
2013-04-14 00:42:51 -0400

    download collabfinder projects

---
+++ collabfinder-project-download.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -e
+
+# This converges to normality by the central limit theorem
+rnorm() {
+  echo $((
+    $(($RANDOM % 6)) +
+    $(($RANDOM % 6)) +
+    $(($RANDOM % 6)) +
+    $(($RANDOM % 6)) +
+    $(($RANDOM % 6)) +
+    $(($RANDOM % 6))
+  ))
+}
+
+dir="collabfinder/$(date --rfc-3339 date)/projects"
+mkdir -p "${dir}"
+nTries=20
+
+failuresInARow=0
+for projectId in $(seq 1 1000); do
+  file="${dir}/${projectId}"
+
+  test -f "${file}" && continue
+
+  wget -O "${file}" "http://collabfinder.com/project/${projectId}" || failuresInARow=$(($failuresInARow + 1))
+  sleep $(rnorm)s
+
+  if test $nTries -lt $failuresInARow; then
+    echo Stopped after non-project number $projectId
+    exit 0
+  fi
+done
