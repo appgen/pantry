8a90cad4fdc123201fb1e2a9f138ce7ee096c743
Thomas Levine <occurrence@thomaslevine.com>
2013-04-15 07:13:54 -0400

    tmp directory

--- socrata-print.sh
+++ socrata-print.sh
@@ -2,6 +2,7 @@
 # Convert the csv files to pdf, take the first page, concatenate them.
 
 # http://ask.libreoffice.org/en/question/13207/filter-options-for-convert-to-using-command-line/
-# rm /tmp/socrata-concat/*.pdf
+mkdir -p /tmp/socrata-concat/
+rm -f /tmp/socrata-concat/*.pdf
 
-unoconv -f pdf -o /tmp/socrata-concat -eSelectPdfVersion=1 socrata/rows/fijd-wye8
+unoconv -f pdf -o /tmp/socrata-concat -eSelectPdfVersion=1 socrata/rows/*
