bb3ca178427e32f3b460ca5de9b5282d94d23cf0
Thomas Levine <occurrence@thomaslevine.com>
2013-04-22 06:22:11 -0400

    attribution links

--- attribution_download.py
+++ attribution_download.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python2
 import os, json
+
 import helpers
 from socrata_datasets_viewids import get_viewids
 
 def get_attribution_links():
     for viewid in get_viewids():
         f = open(os.path.join('socrata', 'views', viewid))
-        attribution = json.load(f).get('attribution')
+        attribution = json.load(f).get('attributionLink')
+        print attribution
         if attribution:
-            print attribution
             yield helpers.get(attribution, cachedir = 'attribution')
