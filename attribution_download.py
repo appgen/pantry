e1c68d1b9a1284bcab9d6fc1ff57e6cae01b6f92
Thomas Levine <occurrence@thomaslevine.com>
2013-04-22 06:26:37 -0400

    attribution download

--- attribution_download.py
+++ attribution_download.py
@@ -4,9 +4,15 @@ import os, json
 import helpers
 from socrata_datasets_viewids import get_viewids
 
+def get_attribution_bodies():
+    'Get the bodies of the attribution links.'
+    for attribution in get_attribution_links():
+        yield (attribution, helpers.get(attribution, cachedir = 'attribution'))
+
 def get_attribution_links():
+    'Get the urls.'
     for viewid in get_viewids():
         f = open(os.path.join('socrata', 'views', viewid))
         attribution = json.load(f).get('attributionLink')
         if attribution:
-            yield helpers.get(attribution, cachedir = 'attribution')
+            yield attribution
