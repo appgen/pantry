45cfdd39817fe10a140dae124fe5aa7f19aeb349
Thomas Levine <occurrence@thomaslevine.com>
2013-04-25 08:17:00 -0400

    wikipedia article download

--- wikipedia.py
+++ wikipedia.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python2
 'Download Wikipedia articles.'
+from urllib2 import urlencode
 
-'http://en.wikipedia.org/w/api.php?format=json&action=query&titles=%s&prop=revisions&rvprop=content'
+import helpers
+
+def article(title):
+    'Download the English Wikipedia article of a given title.'
+    urlbase = 'http://en.wikipedia.org/w/api.php?format=json&action=query&prop=revisions&rvprop=content&'
+    params = urlencode({'titles': title.encode('utf-8')})
+    return helpers.get(urlbase + params, cachedir = 'wikipedia')
