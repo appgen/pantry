94bee0f7c67248d9fedc192265ce4d9a57995918
Thomas Levine <occurrence@thomaslevine.com>
2013-04-26 12:30:10 -0400

    Download maps too

--- socrata_datasets_download.sh
+++ socrata_datasets_download.sh
@@ -4,10 +4,10 @@ set -e
 download_page() {
   [ -z "$1" ] && echo 'You must specify a page number' && return 1
   file="socrata/datasets/$1"
-  test -f "$file" || wget -O "$file" "https://data.cityofnewyork.us/browse?limitTo=datasets&page=$1&sortBy=oldest&view_type=table"
+  test -f "$file" || wget -O "$file" "https://data.cityofnewyork.us/browse?limitTo=datasets|maps&page=$1&sortBy=oldest&view_type=table"
 }
 
 mkdir -p socrata/datasets
-for page in $(seq 1 80); do
+for page in $(seq 1 210); do
   download_page $page
 done
