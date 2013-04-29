b789f820eb66fad4d064d11cbd7f98f6d005222e
Thomas Levine <occurrence@thomaslevine.com>
2013-04-28 20:15:08 -0400

    handle .gz files

--- socrata_rows_s3.sh
+++ socrata_rows_s3.sh
@@ -3,7 +3,7 @@ set -e
 # Upload NYC big apps rows to S3.
 
 s3cmd ls s3://rows.appgen.me|cut -d/ -f4|sort > /tmp/uploaded
-ls socrata/rows/*-*|sed -e 's/.*\///' -e '/\.gz/d' |sort > /tmp/downloaded 
+ls socrata/rows/*-*|sed -e 's/.*\///' -e 's/\.gz//' |sort > /tmp/downloaded 
 
 for viewid in $(diff /tmp/{downloaded,uploaded}|sed 1d|cut -d\  -f2); do
   echo Uploading $viewid
