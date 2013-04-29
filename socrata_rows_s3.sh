4e76551c6cc9ca653f0e1a14df5ae783d3ee5551
Thomas Levine <occurrence@thomaslevine.com>
2013-04-28 20:02:51 -0400

    mime

--- socrata_rows_s3.sh
+++ socrata_rows_s3.sh
@@ -3,10 +3,13 @@ set -e
 # Upload NYC big apps rows to S3.
 
 s3cmd ls s3://rows.appgen.me|cut -d/ -f4|sort > /tmp/uploaded
-ls socrata/rows/*-*|sed -e 's/.*\///' -e 's/\.gz//' |sort > /tmp/downloaded
+ls socrata/rows/*-*|sed -e 's/.*\///' -e '/\.gz/d' |sort > /tmp/downloaded 
 
 for viewid in $(diff /tmp/{downloaded,uploaded}|sed 1d|cut -d\  -f2); do
   echo Uploading $viewid
   gzip "socrata/rows/${viewid}"
-  s3cmd put --add-header=Content-Encoding:gzip --add-header=Content-Type:text/csv "socrata/rows/${viewid}.gz" s3://rows.appgen.me
+  if ! s3cmd put --add-header='Content-Encoding:gzip' --mime-type='text/csv' "socrata/rows/${viewid}.gz" s3://rows.appgen.me; then
+    gunzip "socrata/rows/${viewid}.gz"
+    exit 1
+  fi
 done
